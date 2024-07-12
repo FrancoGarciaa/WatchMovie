03005ea8ee1d80f5be6b8f77dbf5777424929bb9
francogarcia <calderonfranco307@gmail.com>
2024-07-11 22:41:58 -0300

    primeracorrecion

--- js/main.js
+++ js/main.js
@@ -19,50 +19,50 @@ pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar pelicula
 
     switch (pelicula) {
         case 0:
-            alert('Gracias, Vuelva pronto');
+            alert("Gracias, Vuelva pronto");
         break; 
         case 1:
-            let mensajeUno = 'Cartelera:\n\n-BAD BOYS: HASTA LA MUERTE\n-INTENSAMENTE 2\n-MI VILLANO FAVORITO 4\n-TORNADOS\n-BLACK PINK WORLD TOUR BORN PINK';
-            alert(mensajeUno);
+            let primeraOpcion = "Cartelera:\n\n-BAD BOYS: HASTA LA MUERTE\n-INTENSAMENTE 2\n-MI VILLANO FAVORITO 4\n-TORNADOS\n-BLACK PINK WORLD TOUR BORN PINK";
+            alert(primeraOpcion);
         break; 
         case 2:
-            let mensajeDos = 'Peliculas en estreno:\n\n-LA MATRIARCA\n-UN LUGAR EN SILENCIO: DÍA UNO\n-CLUB CERO\n-DEADPOOL & WOLVERINE';
-            alert(mensajeDos);
-            let operacion
-            do {
-            operacion = prompt("Ingrese la operación que desea realizar (+ - * /). Para salir, ingrese la palabra salir");
-            switch (operacion) {
-            case "salir":
-            alert("Gracias, vuelvas prontos!");
-            break;
-            case "+":
-            suma();
-            break;
-            case "-":
-            resta();
-            break;
-default:
-alert("Valor ingresado no es correcto")
-break;
-}
-
-
-} while (operacion !== "salir");
+            let segundaOpcion = "Peliculas en estreno:\n\n-LA MATRIARCA\n-UN LUGAR EN SILENCIO: DÍA UNO\n-CLUB CERO\n-DEADPOOL & WOLVERINE";
+            alert(segundaOpcion);
         break; 
         case 3:
-            let mensajeTres = 'Combos disponibles:\n\n-Balde de Pochoclo + 2 cocas grandes ($7500)\n-2 Bolsas de Pochoclos + 2 cocas grandes ($7000)\n-Nachos con queso + 1 coca grande ($4500)\n-Pizza + 1 coca grande ($5000)\n\n-Ingresar 5 si quiere sumar los precios de los combos';
-            alert(mensajeTres);
-            
-            
+            let terceraOpcion = "Combos disponibles:\n\n1. Balde de Pochoclo + 2 cocas grandes ($7500)\n2. 2 Bolsas de Pochoclos + 2 cocas grandes ($7000)\n3. Nachos con queso + 1 coca grande ($4500)\n4. Pizza + 1 coca grande ($5000)\n\nPara sumar pulse ACEPTAR";
+            alert(terceraOpcion);
+            let operacion
+            do {
+                function suma() {
+                    const combo1 = parseInt(prompt("INGRESE EL PRECIO DEL PRIMER COMBO QUE QUIERA SUMAR\n\n1. Balde de Pochoclo + 2 cocas grandes ($7500)\n2. 2 Bolsas de Pochoclos + 2 cocas grandes ($7000)\n3. Nachos con queso + 1 coca grande ($4500)\n4. Pizza + 1 coca grande ($5000)"));
+                    const combo2 = parseInt(prompt("INGRESE EL PRECIO DEL SEGUNDO COMBO QUE QUIERA SUMAR\n\n1. Balde de Pochoclo + 2 cocas grandes ($7500)\n2. 2 Bolsas de Pochoclos + 2 cocas grandes ($7000)\n3. Nachos con queso + 1 coca grande ($4500)\n4. Pizza + 1 coca grande ($5000)"));
+                    let resultado1 = alert("El  resultado es $" + (combo1 + combo2));
+                    };
+            operacion = prompt("Confirme de sumar combos ingresando el numero 5\n\nPara volver al menu ingrese 6");
+                switch (operacion) {
+                case "6": 
+                    break;
+                case "5":
+                    suma();
+                break;
+                default:
+                    alert("Numero de combo ingresado no es correcto");
+                    break;
+        }
+        } while (operacion !== "6");
             break; 
         case 4:
-            let mensajeCuatro = 'Ventas anticipadas\n\n-Avengers 5\n-Thunderbolts\n-Avatar 3\n-Toy Story 5\n-Tron: Ares';
+            let mensajeCuatro = "Proximamente\n\n-Avengers 5\n-Thunderbolts\n-Avatar 3\n-Toy Story 5\n-Tron: Ares";
             alert(mensajeCuatro);
         break; 
         default:
-            alert('opcion invalida. Ingresa una opcion valida')
+            alert("opcion invalida. Ingresa una opcion valida")
         break;
     }
 
 }while(pelicula !==0);
+
+
+
 
