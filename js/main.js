8252c4fab8cfcd5a660e2271eb75d967c5fd01a7
francogarcia <calderonfranco307@gmail.com>
2024-08-01 22:24:25 -0300

    repo java2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 
 
 const nombre = prompt("Ingresá tu nombre");
+const apellido = prompt("Ingresá tu apellido");
+
+
+const persona1 = {
+    nombre: nombre,
+    apellido: apellido,
+}
 
 function saludarPersona(){
-alert("Bienvenido, " + nombre);
+alert("Bienvenido, " + nombre + " " + apellido);
 };
 
 saludarPersona();
@@ -15,7 +22,7 @@ alert("Bienvenido a WatchMovie");
 
 do {
 
-pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar peliculas recien en estreno\n3. Mostrar combos disponibles\n4. Ventas ancipadas de estrenos\n\nPara salir, ingrese 0"));
+pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar peliculas recien en estreno\n3. Mostrar combos disponibles\n4. Ventas ancipadas de estrenos\n5. Para querer alguna sugerencia\n\nPara salir, ingrese 0"));
 
     switch (pelicula) {
         case 0:
@@ -26,7 +33,7 @@ pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar pelicula
             alert(primeraOpcion);
         break; 
         case 2:
-            let segundaOpcion = "Peliculas en estreno:\n\n-LA MATRIARCA\n-UN LUGAR EN SILENCIO: DÍA UNO\n-CLUB CERO\n-DEADPOOL & WOLVERINE";
+            let segundaOpcion = "Peliculas en estreno:\n\n-LA MATRIARCA\n-UN LUGAR EN SILENCIO: DÍA UNO\n-CLUB CERO\n-DEADPOOL & WOLVERINE\n-El último conjuro";
             alert(segundaOpcion);
         break; 
         case 3:
@@ -39,7 +46,8 @@ pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar pelicula
                     const combo2 = parseInt(prompt("INGRESE EL PRECIO DEL SEGUNDO COMBO QUE QUIERA SUMAR\n\n1. Balde de Pochoclo + 2 cocas grandes ($7500)\n2. 2 Bolsas de Pochoclos + 2 cocas grandes ($7000)\n3. Nachos con queso + 1 coca grande ($4500)\n4. Pizza + 1 coca grande ($5000)"));
                     let resultado1 = alert("El  resultado es $" + (combo1 + combo2));
                     };
-            operacion = prompt("Confirme de sumar combos ingresando el numero 5\n\nPara volver al menu ingrese 6");
+            operacion = prompt("Confirme de sumar combos ingresando el numero 5\n\nPara volver al menu principal ingrese 6");
+
                 switch (operacion) {
                 case "6": 
                     break;
@@ -56,12 +64,85 @@ pelicula = parseInt(prompt("Marca la opcion\n\n1. Cartelera\n2. Mostrar pelicula
             let mensajeCuatro = "Proximamente\n\n-Avengers 5\n-Thunderbolts\n-Avatar 3\n-Toy Story 5\n-Tron: Ares";
             alert(mensajeCuatro);
         break; 
+        case 5:
+            let mensajeCinco = "Se le informa al usuario que tendra que responder preguntas con si/no para recibir algunas sugerencias de nuestra cartelera";
+            alert(mensajeCinco);
+            do {
+                Pregunta1 = parseInt(prompt("¿Te gustan las peliculas de comedia?\n\n1. Si\n2. No\n\n0. Para volver al menu principal"))
+
+
+                switch (Pregunta1) {
+                    case 1:
+                        const primeraSugerencia = ["DEADPOOL & WOLVERINE", "BAD BOYS: HASTA LA MUERTE", "La matriarca"];
+                        primeraSugerencia.forEach((peliculascomedia) => alert(persona1.nombre + " " + persona1.apellido + ", " + "te recomendamos:" + " -" + peliculascomedia));
+                    break;
+                    case 2:
+                        do {
+                            Pregunta2 = parseInt(prompt("¿Te gustan las peliculas de terror?\n\n1. Si\n2. No\n\n0. Para volver atras"))
+
+
+                            switch (Pregunta2) {
+                                case 1:
+                                    const segundaSugerencia = ["UN LUGAR EN SILENCIO: DIA UNO", "El último conjuro"];
+                                    segundaSugerencia.forEach((peliculasterror) => alert(persona1.nombre + " " + persona1.apellido + " " + "te recomendamos:" + " -" + peliculasterror));
+                                break;
+                                case 2:
+                                    do {
+                                        Pregunta3 = parseInt(prompt("¿Te gustan las peliculas animadas?\n\n1. Si\n2. No\n\n0. Para volver atras"))
+
+
+                                        switch (Pregunta3) {
+                                            case 1:
+                                                const terceraSugerencia = ["INTENSAMENTE 2", "MI VILLANO FAVORITO 4"];
+                                                terceraSugerencia.forEach((peliculasanimadas) => alert(persona1.nombre + " " + persona1.apellido + " " + "te recomendamos:" + " -" + peliculasanimadas));
+                                            break;
+                                            case 2:
+                                                if (2) {
+                                                    return(pelicula)
+                                                } else {
+                                                }
+                                                /*alert("Este cine no es para ti, Bye <3!")*/
+                                            break;
+                                            case 0:
+                                                alert("Volviste atras");
+                                            break; 
+                                            default:
+                                                alert("opcion invalida. Ingresa una opcion valida")
+                                            break;
+                                            }
+                                        }
+                                    while (Pregunta1 !==0, Pregunta2 !==0, Pregunta3 !==0);
+                                break;
+                                case 0:
+                                    alert("Volviste atras");
+                                break; 
+                                default:
+                                    alert("opcion invalida. Ingresa una opcion valida")
+                                break;
+                            } 
+                        }
+                        while (Pregunta1 !==0, Pregunta2 !==0);
+                    break;
+                    case 0:
+                        alert("Volviste al menu principal");
+                    break; 
+                    default:
+                        alert("opcion invalida. Ingresa una opcion valida")
+                    break;
+                }
+            } while (Pregunta1 !==0);
+        break; 
         default:
             alert("opcion invalida. Ingresa una opcion valida")
         break;
+    
     }
+}
+while(pelicula !==0);
 
-}while(pelicula !==0);
+
+
+
 
 
 
