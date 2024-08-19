7cd0140266912a590b98d31be3946af0fafec257
francogarcia <calderonfranco307@gmail.com>
2024-08-19 18:55:14 -0300

    Watchcity1

--- js/ingresar.js
+++ js/ingresar.js
@@ -1,24 +1,38 @@
-const nombre = prompt("Ingresá tu nombre");
-const apellido = prompt("Ingresá tu apellido");
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const nick = document.getElementById("nickname")
+const form = document.getElementById("form")
+const parrafo =  document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `el nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value))
+        warnings += `el email no es valido <br>`
+        entrar = true
+    if(nick.value.length < 4){
+        warnings += `el nick no es valido <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
 
 
-const persona1 = {
-    nombre: nombre,
-    apellido: apellido,
-}
+// const nombre = prompt("Ingresá tu nombre");
+// const apellido = prompt("Ingresá tu apellido");
 
-switch (Pregunta3) {
-    case 1:
-        const terceraSugerencia = ["INTENSAMENTE 2", "MI VILLANO FAVORITO 4"];
-        terceraSugerencia.forEach((peliculasanimadas) => alert(persona1.nombre + " " + persona1.apellido + " " + "te recomendamos:" + " -" + peliculasanimadas));
-    break;
-    case 2:
-        alert("Perdon no tenemos lo que buscas. :(")
-    break;
-    case 0:
-        alert("Volviste atras");
-    break; 
-    default:
-        alert("opcion invalida. Ingresa una opcion valida")
-    break;
-    } 
+
+// const persona1 = {
+//     nombre: nombre,
+//     apellido: apellido,
+// }
