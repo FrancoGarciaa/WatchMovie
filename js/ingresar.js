364d884d280f2b4febd8099f112a52bf0b1b7869
francogarcia <calderonfranco307@gmail.com>
2024-09-07 00:03:56 -0300

    Watchcity5

--- js/ingresar.js
+++ js/ingresar.js
@@ -1,5 +1,3 @@
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
 const nick = document.getElementById("nickname")
 const form = document.getElementById("form")
 const parrafo =  document.getElementById("warnings")
@@ -8,25 +6,14 @@ form.addEventListener("submit", e=>{
     e.preventDefault()
     let warnings = ""
     let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += `el nombre no es valido <br>`
-        entrar = true
-    }
-    if(!regexEmail.test(email.value)){
-        warnings += `el email no es valido <br>`
-        entrar = true
-        }
     if(nick.value.length < 4){
         warnings += `el nick no es valido <br>`
         entrar = true
     }
 
     const data = {
-        nombre: nombre.value,
         nickname: nick.value,
-        email: email.value
     }
 localStorage.setItem("usuario", JSON.stringify(data));
 
